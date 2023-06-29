b00e26915d285c9107fd18bf8cdfea5f9906e0a2
kirtondavid53 <kirtondavid53@gmail.com>
2023-06-29 12:34:40 -0400

    second commit

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h+3+^=pfk&k*i5x%u#y9^v+2v-=cuthw17!#^6-u(72mg3gcvo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
